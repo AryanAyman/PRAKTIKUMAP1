80999573d6c0e2aaf3f40a44b18ee17be5a2e43a
AryanAyman <146154223+AryanAyman@users.noreply.github.com>
2023-09-27 10:29:17 +0700

    Add files via upload

---
+++ Pertemuan 2 Aryan/tugasmingguke1aryan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int tugas = 90;
+  int uts = 75;
+  int uas = 80;
+  int total = tugas + uts + uas;
+  int rata2 = 3;
+
+  // cout << total/rata2 << endl << endl;
+
+  cout << "Tugas = " << tugas << endl;
+  cout << "UTS = " << uts << endl;
+  cout << "UAS = " << uas << endl;
+  cout << "Rata-Rata = " << total / rata2 << endl;
+}
---
+++ Pertemuan 2 Aryan/tugasmingguke2aryan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //. Amir pergi dari Jakarta ke Surabaya dengan menempuh jarak 750km. dengan
+//kendaraan roda 4 waktu tempuhnya adalah 20jam, Hitunglah kecepatan kendaraan
+//Amir tersebut !
+  float jarak = 750;
+  float waktu = 20;
+
+  cout << "Kecepatan kendaraan Amir adalah : " << jarak/waktu << " km/h" ;
+}
